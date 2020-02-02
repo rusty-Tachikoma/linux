02ca8b5717223df20b2e7dd441d1478877cf1643
Mauro Rossi <issor.oruam@gmail.com>
2020-02-02 13:01:02 +0100

    drm/amd/display: dc/irq/dce60: fixes for kernel 5.6 cycle
    Changes due to following commit:
    d9e3267 ("drm/amd/display: cleanup of construct and destruct funcs")

--- drivers/gpu/drm/amd/display/dc/irq/dce60/irq_service_dce60.c
+++ drivers/gpu/drm/amd/display/dc/irq/dce60/irq_service_dce60.c
@@ -370,7 +370,7 @@ static const struct irq_service_funcs irq_service_funcs_dce60 = {
 		.to_dal_irq_source = to_dal_irq_source_dce60
 };
 
-static void construct(
+static void dce60_irq_construct(
 	struct irq_service *irq_service,
 	struct irq_service_init_data *init_data)
 {
@@ -389,7 +389,7 @@ struct irq_service *dal_irq_service_dce60_create(
 	if (!irq_service)
 		return NULL;
 
-	construct(irq_service, init_data);
+	dce60_irq_construct(irq_service, init_data);
 	return irq_service;
 }
 
